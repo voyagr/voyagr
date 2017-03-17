7b793aa67a078b205c045e1833200379a31d7a6c
Aria Moraine <ariamoraine@gmail.com>
2017-03-17 10:49:07 -0400

    starting out

---
+++ app/components/Render.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+function pageRender (props) {
+  console.log("PROPS", props)
+  return (
+    <div>
+      <h1>HIZZZ</h1>
+    </div>
+  )
+}
+
+export default connect (
+
+)()
