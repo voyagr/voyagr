74444f776b865b58c88c950b5e6d16e695b8c9de
Aria Moraine <ariamoraine@gmail.com>
2017-03-17 13:55:06 -0400

    working style

--- app/components/Render.jsx
+++ app/components/Render.jsx
@@ -5,20 +5,30 @@ class PageRender extends Component {
 
   constructor (props) {
     super(props)
+    this.parseEle = this.parseEle.bind(this)
+  }
+
+  parseEle (type, ele) {
+    //switch on what type
+    //want to return bootstrap ele with style of x and y
+    switch (type) {
+      case "textBox":
+        return (<p className={`text-${ele.size}`}>{ele.text}</p>)
+    }
   }
 
   render () {
-    console.log("STATE", this.state)
     console.log("PROPS", this.props)
     return (
       <div>
         <h1>HIZZZ</h1>
+        {this.parseEle("textBox", this.props.page.textBox[0])}
       </div>
     )
   }
 }
 
 export default connect (
-  state => ({page: state.page}),
+  state => ({page: state.page.pageInfo.display_info}),
   (dispatch) => ({})
 )(PageRender)
