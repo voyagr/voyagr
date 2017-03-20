15404f6e12f5163292f885fb439eb82d363b4a4b
Aria Moraine <ariamoraine@gmail.com>
2017-03-20 18:30:45 -0400

    added panels to the landing page for sign up and log in

---
+++ app/components/LandingPage.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { PanelGroup, Panel} from 'react-bootstrap'
+import Signup from './Signup'
+export default class LandingPage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeKey: '1'
+    }
+    this.handleSelect = this.handleSelect.bind(this)
+  }
+
+  handleSelect(activeKey) {
+    this.setState({ activeKey });
+  }
+
+  render() {
+    return (
+      <PanelGroup activeKey={this.state.activeKey} onSelect={this.handleSelect} accordion>
+        <Panel header="Sign up" eventKey="1"><Signup/></Panel>
+        <Panel header="Log in" eventKey="2">LOG IN PLACE HOLDER</Panel>
+      </PanelGroup>
+    );
+  }
+}
