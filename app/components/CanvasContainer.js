0459687903bce6a7b3b27a97fe0c7b0c1f21373d
Aria Moraine <ariamoraine@gmail.com>
2017-03-25 17:20:00 -0400

    set tripAction ref correctly so that the trip actions are getting stored in the right place in the database. also set a tripinfo ref to get all trip info

--- app/components/CanvasContainer.js
+++ app/components/CanvasContainer.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import Canvas from './Canvas'
 import { Provider } from 'react-redux'
-import {ref} from 'APP/db/firebase'
+import { database } from 'APP/db/firebase'
 import store from 'APP/app/store'
 import ToolBox from './ToolBox'
 import { Grid, Col } from 'react-bootstrap'
@@ -11,12 +10,28 @@ export default class CanvasContainer extends Component {
   constructor (props) {
     super(props)
   }
+  // export const ref = database.ref()
+  //tripRef = database.ref()
+//  const userId = user.uid
+//  const dbUserPhotosRef = database.ref(`photos/${userId}`)
+//  dbUserPhotosRef.on('value', (snapshot) => this.setState({
+//    photos: snapshot.val(),
+//  }))
+
 
   //when this component mounts, figure out the firebase path from params
   componentDidMount () {
+    const tripId = this.props.params.tripId
+    const tripActionsRef = database.ref(`tripActions/${tripId}`)
+    const tripInfoRef = database.ref(`tripInfo/${tripId}`)
+
     this.setState({
-      store: store(ref.child(this.props.params.tripId))
+      store: store(tripActionsRef)
     })
+
+    tripInfoRef.on('value', (snap) => this.setState({
+      tripInfo: snap.val()
+    }))
   }
 
   //add component will receive props, update store
@@ -24,10 +39,20 @@ export default class CanvasContainer extends Component {
   //possibly add cleanup for component will unmount
 
   render () {
+    console.log("PROPS", this.props)
+    console.log("STATE", this.state)
     if (!this.state) return null
+    let tripInfo = this.state.tripInfo || null
     return (
       <Provider store={this.state.store}>
         <Grid>
+          {
+            tripInfo ?
+            <div>
+              <h1>{`${tripInfo.name}, ${tripInfo.description}, ${tripInfo.startDate}`}</h1>
+            </div>
+            : null
+          }
           <Col lg={4}>
             <ToolBox />
           </Col>
