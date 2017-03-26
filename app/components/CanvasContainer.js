071e44d27669d8f3b01f81cdbb5d47490ebcdfb7
Liz <lizkristinaphillips@gmail.com>
2017-03-26 12:05:56 -0400

    adds a togle button that switches between rendering tool bar or not

--- app/components/CanvasContainer.js
+++ app/components/CanvasContainer.js
@@ -2,40 +2,73 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Canvas from './Canvas'
 import { Provider } from 'react-redux'
-import {ref} from 'APP/db/firebase'
+import {ref, auth} from 'APP/db/firebase'
 import store from 'APP/app/store'
 import ToolBox from './ToolBox'
-import { Grid, Col } from 'react-bootstrap'
+import { Grid, Col, Button } from 'react-bootstrap'
 
 export default class CanvasContainer extends Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      store: null,
+      editable: true,
+    }
+
+    this.toggleMode = this.toggleMode.bind(this)
+    this.renderView = this.renderView.bind(this)
+  }
+
+  toggleMode() {
+    this.setState({
+      editable: !this.state.editable,
+    })
   }
 
   //when this component mounts, figure out the firebase path from params
-  componentDidMount () {
+  componentWillMount () {
     this.setState({
       store: store(ref.child(this.props.params.tripId))
     })
+    //add a auth user listener
+    //if user then check and see if the page belongs to them. Only display edit/view button if page belongs to them
+  //
+  }
+
+  componentWillUnmount () {
+    //add cleanup from auth.userChange listener
   }
 
   //add component will receive props, update store
 
   //possibly add cleanup for component will unmount
 
+  renderView() {
+    return this.state.editable
+            ?  <Grid>
+                <Col lg={4}>
+                  <ToolBox />
+                </Col>
+                <Col lg={8}>
+                  <Canvas />
+                </Col>
+              </Grid>
+            : <Canvas editable={this.state.editable}/>
+  }
+
   render () {
     if (!this.state) return null
+
     return (
-      <Provider store={this.state.store}>
-        <Grid>
-          <Col lg={4}>
-            <ToolBox />
-          </Col>
-          <Col lg={8}>
-            <Canvas />
-          </Col>
-        </Grid>
-      </Provider>
+      <div>
+        <Button onClick={this.toggleMode}>
+          {this.state.editable ? "View" : "Edit" }
+        </Button>
+        <Provider store={this.state.store}>
+          {this.renderView()}
+        </Provider>
+      </div>
     )
   }
 }
