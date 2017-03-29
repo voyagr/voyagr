723eb77d432c67a178a7b15694fadec12c06044c
Claire Freehafer <claire.freehafer@gmail.com>
2017-03-29 17:25:08 -0400

    tabs -> spaces b/c github is lame

--- app/components/ToolBoxComponents/InviteUser.jsx
+++ app/components/ToolBoxComponents/InviteUser.jsx
@@ -4,91 +4,91 @@ import { inviteUser } from 'APP/app/components/utils/inviteUser'
 import { auth } from 'APP/db/firebase'
 
 export default class InviteUser extends Component {
-	constructor(props) {
-		super()
+  constructor(props) {
+    super()
 
-		this.state = {
-			email: '',
-			showInvalidAlert: false,
-			showSuccessAlert: false,
-		}
+    this.state = {
+      email: '',
+      showInvalidAlert: false,
+      showSuccessAlert: false,
+    }
 
-		this.handleChange = this.handleChange.bind(this)
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleInvalidEmail = this.handleInvalidEmail.bind(this)
-		this.handleSuccess = this.handleSuccess.bind(this)
-	}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInvalidEmail = this.handleInvalidEmail.bind(this)
+    this.handleSuccess = this.handleSuccess.bind(this)
+  }
 
-	handleChange (event) {
-		this.setState({
-			email: event.target.value,
-			showInvalidAlert: false,
-			showSuccessAlert: false,
-	 })
-	}
+  handleChange (event) {
+    this.setState({
+      email: event.target.value,
+      showInvalidAlert: false,
+      showSuccessAlert: false,
+   })
+  }
 
-	handleSubmit (event) {
-		event.preventDefault()
-		console.log(event)
-		inviteUser(this.state.email, this.props.tripId)
-			.then(status => {
-				if (status) {
-					this.setState({ showInvalidAlert: true, })
-				} else {
-					this.setState({ showSuccessAlert: true, })
-				}
-			})
-	}
+  handleSubmit (event) {
+    event.preventDefault()
+    console.log(event)
+    inviteUser(this.state.email, this.props.tripId)
+      .then(status => {
+        if (status) {
+          this.setState({ showInvalidAlert: true, })
+        } else {
+          this.setState({ showSuccessAlert: true, })
+        }
+      })
+  }
 
-	handleInvalidEmail () {
-		return (
-			<Alert bsStyle="danger">
-				<p>There is no user registered with that email address.</p>
-			</Alert>
-		)
-	}
+  handleInvalidEmail () {
+    return (
+      <Alert bsStyle="danger">
+        <p>There is no user registered with that email address.</p>
+      </Alert>
+    )
+  }
 
-	handleSuccess () {
-		return (
-			<Alert bsStyle="success">
-				<p>User can now collaborate on this trip.</p>
-			</Alert>
-		)
-	}
+  handleSuccess () {
+    return (
+      <Alert bsStyle="success">
+        <p>User can now collaborate on this trip.</p>
+      </Alert>
+    )
+  }
 
-	render () {
-		return (
-			<div>
-				<h4>Invite To Edit</h4>
-				<Form horizontal onSubmit={this.handleSubmit}>
-					<FormGroup controlId="email">
-						<Col componentClass={ControlLabel} sm={3}>
-							Email
-						</Col>
-						<Col sm={9}>
-							<FormControl
-								type="email"
-								name="email"
-								placeholder="friend@flashback.biz"
-								onChange={this.handleChange}
-							/>
-							<HelpBlock>
-								The email must already be associated with a Voyagr account.
-							</HelpBlock>
-						</Col>
-					</FormGroup>
+  render () {
+    return (
+      <div>
+        <h4>Invite To Edit</h4>
+        <Form horizontal onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email">
+            <Col componentClass={ControlLabel} sm={3}>
+              Email
+            </Col>
+            <Col sm={9}>
+              <FormControl
+                type="email"
+                name="email"
+                placeholder="friend@flashback.biz"
+                onChange={this.handleChange}
+              />
+              <HelpBlock>
+                The email must already be associated with a Voyagr account.
+              </HelpBlock>
+            </Col>
+          </FormGroup>
 
-					<FormGroup>
-						<Col smOffset={3} sm={10}>
-							<Button type="submit" value="Submit">
-								Invite
-							</Button>
-						</Col>
-					</FormGroup>
-					{this.state.showInvalidAlert ? this.handleInvalidEmail() : null}
-					{this.state.showSuccessAlert ? this.handleSuccess() : null}
-				</Form>
-			</div>
-		)
-	}
+          <FormGroup>
+            <Col smOffset={3} sm={10}>
+              <Button type="submit" value="Submit">
+                Invite
+              </Button>
+            </Col>
+          </FormGroup>
+          {this.state.showInvalidAlert ? this.handleInvalidEmail() : null}
+          {this.state.showSuccessAlert ? this.handleSuccess() : null}
+        </Form>
+      </div>
+    )
+  }
 }
